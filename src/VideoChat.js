ce1f97d251302c97a68bd2cea008dacc5231edb6
Masumi Kawasaki <mkawasaki@sbihg.co.jp>
2020-04-04 02:10:25 +0900

    Add video room

--- src/VideoChat.js
+++ src/VideoChat.js
@@ -1,49 +1,46 @@
 import React, { useState, useCallback } from 'react'
 import Lobby from './Lobby'
+import Room from './Room'
 
 const VideoChat = () => {
   const [username, setUsername] = useState('')
   const [roomName, setRoomName] = useState('')
   const [token, setToken] = useState(null)
 
-  const handleUsernameChange = useCallback(event => {
+  const handleUsernameChange = useCallback((event) => {
     setUsername(event.target.value)
   }, [])
 
-  const handleRoomNameChange = useCallback(event => {
+  const handleRoomNameChange = useCallback((event) => {
     setRoomName(event.target.value)
   }, [])
 
   const handleSubmit = useCallback(
-    async event => {
+    async (event) => {
       event.preventDefault()
       const data = await fetch('/video/token', {
         method: 'POST',
         body: JSON.stringify({
           identity: username,
-          room: roomName
+          room: roomName,
         }),
         headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(res => res.json())
+          'Content-Type': 'application/json',
+        },
+      }).then((res) => res.json())
       setToken(data.token)
     },
     [username, roomName]
   )
 
-  const handleLogout = useCallback(event => {
+  const handleLogout = useCallback((event) => {
     setToken(null)
   }, [])
 
   let render
   if (token) {
     render = (
-      <div>
-        <p>Username: {username}</p>
-        <p>Room name: {roomName}</p>
-        <p>Token: {token}</p>
-      </div>
+      <Room roomName={roomName} token={token} handleLogout={handleLogout} />
     )
   } else {
     render = (
