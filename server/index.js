cc636b0634a6102bbfdd87aca7fd4f01c3696d8f
Masumi Kawasaki <701242+geeknees@users.noreply.github.com>
2020-04-04 02:15:28 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,13 +31,6 @@ app.get('/video/token', (req, res) => {
   sendTokenResponse(token, res);
 });
 
-app.post('/video/token', (req, res) => {
-  const identity = req.body.identity;
-  const room = req.body.room;
-  const token = videoToken(identity, room, config);
-  sendTokenResponse(token, res);
-});
-
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
 );
